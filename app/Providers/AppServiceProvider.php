9391a7a4a672c08acf01cca4a77e5b38d90daef2
junaidjanjua08 <junaidjanjua08@gmail.com>
2025-04-29 15:48:53 +0500

    db migrations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Services\InviteService;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Schema::defaultStringLength(125);
     }
 }
